ce14e7cc9f022ecdeb1765c76219cdebce16a21c
Dennis <gomesvelloso@gmail.com>
2021-02-01 16:17:10 -0300

    Subindo Projeto

---
+++ src/Controller/OlaMundoController.php
@@ -0,0 +1,32 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class OlaMundoController
+{
+    /**
+     * @Route("/ola")
+     */
+    public function olaMundoAction(Request $request): Response
+    {
+        //Se eu quiser saber qual foi a url q fez a requisicao
+        $pathInfo  = $request->getPathInfo();
+        //Se quiser buscar somente um parametro
+        $parametro = $request->query->get("tic");
+        //Se quiser buscar todos os parametros de uma vez.
+        $todosParametros = $request->query->all();
+
+        return new JsonResponse(["mensagem"  => "Ola Mundo",
+                                 "pathInfo"  => $pathInfo,
+                                 "parametro" => $parametro,
+                                 "todosParametros" => $todosParametros
+                                ]);
+    }
+}
