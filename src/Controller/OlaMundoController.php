c166d3feb57cb4cb38fd7e773c5142bb86b95d87
Dennis <gomesvelloso@gmail.com>
2021-02-05 00:06:29 -0300

    adicionado o JsonResponse com JSON_PRETTY PRINT

--- src/Controller/OlaMundoController.php
+++ src/Controller/OlaMundoController.php
@@ -22,14 +22,19 @@ class OlaMundoController
         $parametro = $request->query->get("tic");
         //Se quiser buscar todos os parametros de uma vez.
         $todosParametros = $request->query->all();
-        $html = '<div style="width: 99%; height: 25%; padding:5px; margin-bottom: 40px">
+        $html = '<html>
+                    <head>
+                    <meta name="viewport" content="width=device-width, initial-scale=1">
+                    </head>
+                    <body>
+                 </html><div style="width: 99%; height: 25%; padding:5px; margin-bottom: 40px">
                     <div style="margin-top: 60px">
                         <ul>
                             <li><a style="font-weight: bold; outline: none; color: #000; text-decoration: none" href="/">Home</a></li> 
                             <li><a style="font-weight: bold; outline: none; color: #000; text-decoration: none" href="/medicos">Médicos</a></li></ul>
                         </div>
                         <hr>
-                 </div>';
+                 </div></body></html>';
         return new Response($html);
     }
 }
